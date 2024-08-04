712218cde954900c297ebe4dd0fc11227851c856
akshayjthummar <akshaythummar34@gmail.com>
2024-08-04 14:27:56 +0530

    add authentication

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import caregoryRouter from "./category/category-router";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -5,12 +5,18 @@ import logger from "../config/logger";
 import categoryValidator from "./category-validator";
 
 import { asyncWrapper } from "../common/utils/wrapper";
+import authenticate from "../common/middlewares/authenticate";
 
 const router = Router();
 
 const categoryService = new CategoryService();
 const categoryController = new CategoryController(categoryService, logger);
 
-router.post("/", categoryValidator, asyncWrapper(categoryController.create));
+router.post(
+    "/",
+    authenticate,
+    categoryValidator,
+    asyncWrapper(categoryController.create),
+);
 
 export default router;
